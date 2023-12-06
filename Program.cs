4fec69cf7081c7100cffdcc2ab6ecd5d876a6801
Albin <albiv.forsberg2@elev.ga.ntig.se>
2023-12-06 11:07:41 +0100

    lbhliby

--- Program.cs
+++ Program.cs
@@ -13,30 +13,62 @@ Vector2 movement = new Vector2(5, 5);
 Color red = new Color(255, 0, 0, 255);
 
 Rectangle playerRect = new Rectangle(300, 400, 64, 64);
-Rectangle enemyRect = new Rectangle (200, 200, 64, 64);
+Rectangle enemyRect = new Rectangle(200, 200, 64, 64);
 
 Rectangle overlap = Raylib.GetCollisionRec(playerRect, enemyRect);
+
+int currentLevel = 1;
+
 while (!Raylib.WindowShouldClose())
 {
     // position += movement;
+    if (currentLevel == 1 || currentLevel == 2 || currentLevel == 3 )
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            playerRect.x += movement.X;
+        }
 
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)){
-        playerRect.x += movement.X;
-        
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            playerRect.x -= movement.X;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+        {
+            playerRect.y -= movement.Y;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+        {
+            playerRect.y += movement.Y;
+        }
+
+
     }
 
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)){
-        playerRect.x -= movement.X;
-    }
+    if (currentLevel == 1)
+    {
+        if (Raylib.CheckCollisionRecs(playerRect, enemyRect))
+        {
+            System.Console.WriteLine("Level 2");
+            currentLevel = 2;
+            playerRect.x = 10;
 
-    if(Raylib.IsKeyDown(KeyboardKey.KEY_UP)){
-        playerRect.y -= movement.Y;
+        }
     }
+    else if (currentLevel == 2)
+    {
+        if (Raylib.CheckCollisionRecs(playerRect, enemyRect))
+        {
 
-    if(Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)){
-        playerRect.y += movement.Y;
+            System.Console.WriteLine("Level 3");
+            currentLevel = 3;
+            playerRect.x = 20;
+            playerRect.y = 20;
+        }
     }
-
+    
 
 
 
@@ -47,13 +79,36 @@ while (!Raylib.WindowShouldClose())
 
     // x++;
     Raylib.BeginDrawing();
-    Raylib. ClearBackground(Color.GOLD);
+    Raylib.ClearBackground(Color.GOLD);
 
-    
-    Raylib.DrawRectangleRec(playerRect, red);
-    Raylib.DrawRectangleRec(enemyRect, Color.BLACK);
-    Raylib.DrawRectangleRec(overlap, Color.ORANGE);
-    
+    if (currentLevel == 1)
+    {
+
+        Raylib.DrawRectangleRec(playerRect, red);
+
+
+        Raylib.DrawRectangleRec(enemyRect, Color.BLACK);
+
+    }
+
+    else if (currentLevel == 2)
+    {
+        Raylib.DrawRectangleRec(playerRect, Color.DARKGREEN);
+
+        Raylib.DrawRectangleRec(enemyRect, Color.BLACK);
+
+
+    }
+
+    else if (currentLevel == 3)
+    {
+        Raylib.DrawRectangleRec(playerRect, Color.BLUE);
+
+        Raylib.DrawRectangleRec(enemyRect, Color.BLACK);
+    }
+
+    // Raylib.DrawRectangleRec(overlap, Color.ORANGE);
+
     Raylib.EndDrawing();
 
 }
